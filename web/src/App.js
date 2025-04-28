06ec90cf9337327bdf8884c6d6522c5bf537cda0
khang.huynh <dellta103@gmail.com>
2025-04-29 01:13:37 +0700

    implement map

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,34 @@
-import { Route, Routes } from "react-router-dom";
-import { HomePage } from "./pages";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { HomePage, ManagePage } from "./pages";
 import "./App.scss";
+import { Segmented } from "antd";
 
 function App() {
+  const navigate = useNavigate();
+
+  const handleNavigate = (value) => {
+    switch (value) {
+      case "Home":
+        navigate("/");
+        break;
+
+      case "Manage":
+        navigate("/manage");
+        break;
+
+      default:
+        break;
+    }
+  };
   return (
     <div className="app">
       <div className="content">
+        <div className="segmented-container">
+          <Segmented options={["Home", "Manage"]} onChange={handleNavigate} />
+        </div>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/manage" element={<ManagePage />} />
         </Routes>
       </div>
     </div>
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,1 +1,2 @@
 export { default as HomePage } from "./home/home.page";
+export { default as ManagePage } from "./manage/manage.page";
